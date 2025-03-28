43d9aa634300f4cc1bafe48079d15c8eca16c1b2
SeboskiHUb <cuentadeclaseshd@gmail.com>
2025-03-28 11:17:47 -0500

    Creacios ejercicio ParImpar

---
+++ 01/01/Program.cs
@@ -0,0 +1,23 @@
+﻿do
+{
+    Console.Write("Ingrese un numero entero: ");
+    var numero = (Console.ReadLine());
+
+    if (int.TryParse(numero, out int numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numero} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numero} es impar");
+        }
+        return numberInt;
+    }
+    else
+    {
+        Console.WriteLine("Hay que leer el enunciado, ingresa un numero");
+    }
+} while (true);
+

4d6b6263c51f5b85b88ca883be92cbb4b2ef62eb
SeboskiHUb <cuentadeclaseshd@gmail.com>
2025-04-11 10:16:13 -0500

    primer y segundo ejercicio

---
+++ ConceptosDeProgrmacion/NumeroMayor/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Ingrese un numero: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese otro numero: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("El primer numero es mayor");
+}
+else if (num2 > num1)
+{
+    Console.WriteLine("El segundo numero es mayor");
+}
+else
+{
+    Console.WriteLine("Los numero son iguales");
+}
